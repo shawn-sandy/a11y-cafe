565807f930cc47986db78d96956615183757fbbe
shawn <shawnsandy04@gmail.com>
2024-04-28 17:44:07 -0400

    feat: add patterns section and collection
    - add new "Patterns" link to the Navigation component
    - define schema for patterns collection in content config
    - add extra line in 7-patterns.mdx file

--- src/content/config.ts
+++ src/content/config.ts
@@ -36,8 +36,30 @@ const content = defineCollection({
 });
 
 const patterns = {
-  ...postsCollection.schema,
   schema: z.object({
+    title: z.string(),
+    pubDate: z.date(),
+    description: z.string(),
+    author: z.string(),
+    breadcrumbSlug: z.string().optional(), // should match the slug
+    image: z
+      .object({
+        url: z.string(),
+        alt: z.string(),
+        caption: z.string().optional(),
+      })
+      .optional(),
+    tags: z.array(z.string()).optional(),
+    draft: z.boolean().default(false),
+    featured: z.boolean().default(false),
+    youtube: z
+      .object({
+        id: z.string(),
+        title: z.string().optional(),
+        start: z.string().optional(),
+        end: z.string().optional(),
+      })
+      .optional(),
     a11yLinks: z
       .array(
         z.object({
