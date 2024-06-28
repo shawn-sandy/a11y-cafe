068f099cf2a23f41cb6810c97917c9054a656362
shawn <shawnsandy04@gmail.com>
2024-06-28 19:57:31 -0400

    refactor: Update search.json.ts to return JSON with correct items structure

--- src/pages/search.json.ts
+++ src/pages/search.json.ts
@@ -4,25 +4,8 @@ import { json } from "stream/consumers";
 
 export async function GET(context) {
   const blog = await getCollection('posts');
-  // return rss({
-  //   title: 'Buzz’s Blog',
-  //   description: 'A humble Astronaut’s guide to the stars',
-  //   site: context.site,
-  //   items: blog.map((post) => ({
-  //     title: post.data.title,
-  //     pubDate: post.data.pubDate,
-  //     description: post.data.description,
-  //     customData: post.data.customData,
-  //     // Compute RSS link from post `slug`
-  //     // This example assumes all posts are rendered as `/blog/[slug]` routes
-  //     link: `/blog/${post.slug}/`,
-  //   })),
-  // });
 
-  const jsonData = {
-    title: 'Buzz’s Blog',
-    description: 'A humble Astronaut’s guide to the stars',
-    site: context.site,
+  const jsonData = [{ 
     items: blog.map((post) => ({
       title: post.data.title,
       pubDate: post.data.pubDate,
@@ -32,9 +15,14 @@ export async function GET(context) {
       // This example assumes all posts are rendered as `/blog/[slug]` routes
       link: `/posts/${post.slug}/`,
     })),
-  };
+  }];
 
   return new Response(
-    JSON.stringify(jsonData)
+    JSON.stringify(jsonData),  {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
   )
 }
