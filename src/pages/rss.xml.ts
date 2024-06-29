7fb2b2275c24243514fe98d0bb0097fa52e9279a
shawn <shawnsandy04@gmail.com>
2024-06-28 21:07:58 -0400

    refactor: update rss.xml.ts

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -4,6 +4,9 @@ import { getCollection } from 'astro:content';
 export async function GET(context) {
   const blog = await getCollection('posts');
   return rss({
+    title: 'Buzz’s Blog',
+    description: 'A humble Astronaut’s guide to the stars',
+    site: context.site,
     items: blog.map((post) => ({
       title: post.data.title,
       pubDate: post.data.pubDate,
