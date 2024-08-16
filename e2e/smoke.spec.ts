751c34225abcd1027da83742abef4b0cacce4869
shawn <shawnsandy04@gmail.com>
2024-08-16 15:56:04 -0400

    chore: Update Playwright test configuration

--- e2e/smoke.spec.ts
+++ e2e/smoke.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 
 test("smoke test", async ({ page }) => {
-  await page.goto("./");
+  await page.goto("http://localhost:4321/");
   expect(true).toBe(true);
 });
